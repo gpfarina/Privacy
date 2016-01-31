803ac799c141bd8bd3e0f353242322de290ce0cb
gpietro <farinagianpietro@gmail.com>
2016-01-31 17:40:28 -0500

    exp function is working now

--- samplesUnit.r
+++ samplesUnit.r
@@ -55,7 +55,8 @@ sampleFromSimplex<-function(m,nPoints, sizedb,prior, eps){
 #It then performs postprocessing (LP and Hell) if the
 #posterior noisy distributions are closer than distX to the real increases teh relative counters.
 
-
+#v: if TRUE compares  LP(black) vs Hell(red)
+# if FALSE compares   LP(black) vs no-Post(red)
 
 
 exp<-function(nPoints, m, sizedb, prior, eps, distX, v){
@@ -63,32 +64,41 @@ exp<-function(nPoints, m, sizedb, prior, eps, distX, v){
     prob<-matrix(rep(0, nPoints*cat), ncol=cat)
     less1<-rep(0, nPoints)
     less2<-rep(0, nPoints)
+    less3<-rep(0, nPoints)
     for(i in 1:nPoints){
         prob[i,]<- simplex.sample(cat, 1, sort=FALSE)$samples[1,]
         db<-genDbD(sizedb, cat, prob[i,])
         postReal <- computePost(prior, db)
          for(l in 1:m){
-            noisy <-laplaceNoise(2, eps, prior, db)
+            noisy  <-laplaceNoise(2, eps, prior, db)
             noisy1<-laplaceNoiseLPD(2, eps, prior, db, noisy)
-            noisy2<-laplaceNoisePostHellingerD(2, eps, prior, db, noisy)
             if(as.numeric(hellingerD(noisy1, postReal ))<=distX){less1[i]<-less1[i]+1}
-            if(as.numeric(hellingerD(noisy2, postReal ))<=distX){less2[i]<-less2[i]+1}
-            if(as.numeric(hellingerD(noisy  , postReal ))<=distX){less3[i]<-less3[i]+1}
+             if(v){
+                noisy2<-laplaceNoisePostHellingerD(2, eps, prior, db, noisy)
+                if(as.numeric(hellingerD(noisy2, postReal ))<=distX){less3[i]<-less3[i]+1}
+            }
+             else{
+              	if(!(sum(noisy<0)>0)){
+                    if(as.numeric(hellingerD(noisy  , postReal ))<=distX){
+                        less2[i]<-less2[i]+1
+                     }
+                }
+                else
+                    if(sum(noisy<0)>0){show(noisy)}
+            }
         }
         show(i)
     }
-
-data1<-rbind(cbind(prob, less1), cbind(prob, less2))
-data2<-rbind(cbind(prob, less1), cbind(prob, less3))
-print("--------")
-    show(data1)
-print("--------")
-    show(data2)
-if(v){
-    r<-plot3d(x=data1[,1], y=data1[,2], z=data1[,3], xlab="prob", ylab="prob", zlab="Hell/LP", col=c("black", "red"))
-}
-else{
-    r<-plot3d(x=data2[,1], y=data2[,2], z=data2[,3], xlab="prob", ylab="prob", zlab="LP/NoPost", col=c("black", "red"))
-}
-return(r)
+    less1<-(less1/m)*100
+    less2<-(less2/m)*100
+    less3<-(less3/m)*100
+    if(v){
+        data<-rbind(cbind(prob, less1),  cbind(prob, less3))
+    }
+    else{
+        data<-rbind(cbind(prob, less1), cbind(prob, less2))
+    }
+    show(data)
+    r<-plot3d(x=data[,1], y=data[,2], z=data[,3], xlab="prob", ylab="prob", zlab=sprintf("#<=%.1f",distX), col=c(rep("black",nPoints), rep("red",nPoints)))
+    return(r)
 }
